28d08894566ffd79b83415bd86d4b45ea9a27a36
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-01-29 13:25:45 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="swiftsimio",
+    version="0.1.0",
+    description="SWIFTsim (swift.dur.ac.uk) i/o routines for python."
+,
+    url="https://gitlab.cosma.dur.ac.uk/jborrow/SWIFTsimIO",
+    author="Josh Borrow",
+    author_email="joshua.borrow@durham.ac.uk",
+    packages=["swiftsimio"],
+    zip_safe=False
+)
+
