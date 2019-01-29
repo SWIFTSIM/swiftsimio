94292e3799c5c5ba6d8b1b5c9cf3804191fb56a8
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-01-29 14:22:45 +0000

    Added new snapshot information and units

--- swiftsimio/metadata/particle/particle_fields.py
+++ swiftsimio/metadata/particle/particle_fields.py
@@ -7,6 +7,23 @@ shared = {
     "Masses": "masses",
     "ParticleIDs": "particle_ids",
     "Velocities": "velocities",
+    "Potential": "potential",
+}
+
+baryon = {
+    "ElementAbundance": "element_abundance",
+    "Maximal Temperature": "maximal_temperature",
+    "Maximal Temperature scale-factor": "maximal_temperature_scale_factor",
+    "IronMassFracFromSNIa": "iron_mass_frac_from_sn1a",
+    "MetalMassFracFromAGB": "metal_mass_frac_from_agb",
+    "MetalMassFracFromSNII": "metal_mass_frac_from_snii",
+    "MetalMassFracFromSNIa": "metal_mass_frac_from_sn1a",
+    "Metallicity": "metallicity",
+    "SmoothedElementAbundance": "smoothed_element_abundance",
+    "SmoothedIronMassFracFromSNIa": "smoothed_iron_mass_frac_from_sn1a",
+    "SmoothedMetallicity": "smoothed_metallicity",
+    "TotalMassFromAGB": "total_mass_from_agb",
+    "TotalMassFromSNII": "total_mass_from_snii",
 }
 
 gas = {
@@ -15,11 +32,24 @@ gas = {
     "InternalEnergy": "internal_energy",
     "SmoothingLength": "smoothing_length",
     "Pressure": "pressure",
+    "Diffusion": "diffusion",
+    "SFR": "sfr",
+    "Temperature": "temperature",
+    "Viscosity": "viscosity",
+    "sSFR": "specific_sfr",
     **shared,
+    **baryon,
 }
 
 dark_matter = {**shared}
 
-stars = {**shared}
+stars = {
+    "BirthDensity": "birth_density",
+    "Birth_time": "birth_time",
+    "Initial_Masses": "initial_masses",
+    "NewStarFlag": "new_star_flag",
+    **shared,
+    **baryon,
+}
 
 black_holes = {**shared}
--- swiftsimio/metadata/unit/unit_fields.py
+++ swiftsimio/metadata/unit/unit_fields.py
@@ -22,18 +22,40 @@ def generate_units(m, l, t, I, T):
 
     shared = {"coordinates": l, "masses": m, "particle_ids": None, "velocities": l / t}
 
+    baryon = {
+        "element_abundance": None,
+        "maximal_temperature": T,
+        "maximal_temperature_scale_factor": None,
+        "iron_mass_frac_from_sn1a": None,
+        "metal_mass_frac_from_agb": None,
+        "metal_mass_frac_from_snii": None,
+        "metal_mass_frac_from_sn1a": None,
+        "metallicity": None,
+        "smoothed_element_abundance": None,
+        "smoothed_iron_mass_frac_from_sn1a": None,
+        "smoothed_metallicity": None,
+        "total_mass_from_agb": m,
+        "total_mass_from_snii": m,
+    }
+
     gas = {
         "density": m / (l ** 3),
         "entropy": m * l ** 2 / (t ** 2 * T),
         "internal_energy": m * l ** 2 / (t ** 2),
         "smoothing_length": l,
         "pressure": m / (l * t ** 2),
+        "diffusion": None,
+        "SFR": m / t,
+        "temperature": T,
+        "viscosity": None,
+        "specific_sfr": 1 / t,
         **shared,
+        **baryon,
     }
 
     dark_matter = {**shared}
 
-    stars = {**shared}
+    stars = {**shared, **baryon}
 
     black_holes = {**shared}
 
