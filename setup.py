cae62db31dcfa697ac3346ef5755cb6d6aa694f1
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-01-31 13:27:20 +0000

    Added license and setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="swiftsimio",
     version="0.1.0",
-    description="SWIFTsim (swift.dur.ac.uk) i/o routines for python."
-,
+    description="SWIFTsim (swift.dur.ac.uk) i/o routines for python.",
     url="https://gitlab.cosma.dur.ac.uk/jborrow/SWIFTsimIO",
     author="Josh Borrow",
     author_email="joshua.borrow@durham.ac.uk",
     packages=["swiftsimio"],
-    zip_safe=False
+    long_description=long_description,
+    zip_safe=False,
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: LGPLv3 License",
+        "Operating System :: OS Independent",
+    ],
 )
 
