b22725b7dd0d6c145bd23651a1c5b299d1c5912a
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-02-04 11:49:37 +0000

    Added a validation function to check if SWIFT files are valid outputs

--- swiftsimio/__init__.py
+++ swiftsimio/__init__.py
@@ -6,12 +6,27 @@ import swiftsimio.metadata as metadata
 name = "swiftsimio"
 
 
+def validate_file(filename):
+    """
+    Checks that the provided file is a SWIFT dataset.
+    """
+    try:
+        with h5py.File(filename, "r") as handle:
+            if handle["Code"].attrs["Code"] != b"SWIFT":
+                raise KeyError
+    except KeyError:
+        raise IOError("File is not of SWIFT data type")
+
+    return True
+
+
 def load(filename) -> SWIFTDataset:
     """
     Loads the SWIFT dataset at filename.
     """
 
     return SWIFTDataset(filename)
+
 
 # Rename this object to something simpler.
 Writer = SWIFTWriterDataset
