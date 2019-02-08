6ea13cefdb6e3715a2dc7679d50c58771225881e
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-02-08 16:23:05 +0000

    Version 0.2.2 of the code!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swiftsimio",
-    version="0.2.1",
+    version="0.2.2",
     description="SWIFTsim (swift.dur.ac.uk) i/o routines for python.",
     url="https://gitlab.cosma.dur.ac.uk/jborrow/SWIFTsimIO",
     author="Josh Borrow",
