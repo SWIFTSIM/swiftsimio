Minor masking bug

swiftsimio/masks.py
@@ -206,7 +206,7 @@ class SWIFTMask(object):
             offsets = self.offsets[ptype][cell_mask]
 
             this_mask = [
-                [c, c+o-1] for c, o in zip(counts, offsets)
+                [o, c+o-1] for c, o in zip(counts, offsets)
             ]
 
             setattr(self, ptype, np.array(this_mask))