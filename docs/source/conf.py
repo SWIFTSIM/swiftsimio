9c7f92397226fe72302bed3ce79f577cb22de2f6
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-11-24 13:03:19 +0000

    Added automatic API docs to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+import swiftsimio
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +24,7 @@ copyright = "2019, Josh Borrow"
 author = "Josh Borrow"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = swiftsimio.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +32,16 @@ release = "2.0.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["recommonmark"]
+extensions = [
+    "recommonmark",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.doctest",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -40,16 +51,48 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+source_suffix = [".rst", ".md"]
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "readthedocs"
+html_theme = "default"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+
+# -- Options for automatic API doc
+
+autodoc_member_order = "bysource"
+autodoc_default_flags = ["members"]
+autosummary_generate = True
+
+
+def run_apidoc(_):
+    try:
+        from sphinx.ext.apidoc import main
+    except ImportError:
+        from sphinx.apidoc import main
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+
+    api_doc_dir = os.path.join(cur_dir, "modules")
+    module = os.path.join(cur_dir, "../..", "swiftsimio")
+    ignore = [
+        os.path.join(cur_dir, "../..", "tests"),
+        os.path.join(cur_dir, "../..", "swiftsimio/metadata"),
+    ]
+
+    os.environ["SPHINX_APIDOC_OPTIONS"] = "members,undoc-members,show-inheritance"
+    main(["-M", "-f", "-e", "-T", "-d 0", "-o", api_doc_dir, module, *ignore])
+
+
+def setup(app):
+    app.connect("builder-inited", run_apidoc)
