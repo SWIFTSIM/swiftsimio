cf5522d59f04acb855978cbd4722131ebc3f549a
Alexei Borissov <dc-bori1@login7c.pri.cosma7.alces.network>
2020-04-23 17:07:34 +0100

    read dataset from snapshot

--- swiftsimio/subset_writer.py
+++ swiftsimio/subset_writer.py
@@ -10,16 +10,30 @@ import unyt
 import h5py
 import numpy as np
 
-#class SWIFTDatasubset(object):
-#    """
-#    class for reading in subset of SWIFT snapshot
-#    """
-#
-#    def __init__(self, filename: str):
-#        # Load the units and metadata
-#        #self.units = SWIFTUnits(filename)
-#        #self.metadata = SWIFTMetadata(filename, self.units)
-#        with h5py.File(filename, "r") as infile:
+class SWIFTDatasubset(object):
+    """
+    class for reading in subset of SWIFT snapshot
+    """
+
+    def __init__(self, filename: str):
+        # Load the units and metadata
+        self.filename = filename
+        self.dataset_names = []
+        self.datasets = []
+        
+        with h5py.File(filename, "r") as infile:
+            # Find all the datasets in the snapshot
+            infile.visititems(self.find_datasets)
+
+    def find_datasets(self, name, node):
+        if isinstance(node, h5py.Dataset) and not name in self.dataset_names:
+            self.dataset_names.append(name)
+
+    def load_datasets(self):
+        with h5py.File(self.filename, "r") as infile:
+            for i in range(2):
+                dataset = infile.get(self.dataset_names[i])[()]
+                self.datasets.append(dataset)
             
 
 
@@ -36,6 +50,12 @@ class SWIFTWriterDatasubset(object):
         self.write_metadata()
 
     def write_metadata(self):
+        """
+        Copy over all the metadata from snapshot to output file
+
+        ALEXEI: rewrite this taking advantage of finding the 
+        datasets in SWIFTDatasubset
+        """
         with h5py.File(self.input_filename, "r") as input_file:
             with h5py.File(self.output_filename, "w") as output_file:
                 for field in input_file.keys():
