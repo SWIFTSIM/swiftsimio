8665534a597b7968e8db7ec75dd362ea9232f4cc
Alexei Borissov <dc-bori1@login7c.pri.cosma7.alces.network>
2020-04-28 15:43:01 +0100

    debugging

---
+++ subset_write_test.py
@@ -0,0 +1,53 @@
+import numpy as np
+from swiftsimio.subset_writer import SWIFTWriterDatasubset, SWIFTDatasubset
+import swiftsimio as sw
+import h5py
+import sys
+
+def compare(A, B):
+    val = True
+    for key in A.__dict__.keys():
+        print("comparing ", key)
+        try:
+            if A.__dict__[key] != B.__dict__[key] :
+                val = False
+        except TypeError:
+            if not all(A.__dict__[key][:] == B.__dict__[key][:]):
+                val = False
+        except:
+            print(A.__dict__[key])
+
+    if val:
+        print("everything's fine")
+    else:
+        print("something's wrong")
+        sys.exit()
+
+
+# Specify filepaths
+infile = "/cosma7/data/dp004/dc-bori1/swiftsimio_project/snapshots/test_snap_eagle25.hdf5"
+outfile = "/cosma7/data/dp004/dc-bori1/swiftsimio_project/snapshots/test_out.hdf5"
+
+# Create a mask
+mask = sw.mask(infile)
+
+boxsize = mask.metadata.boxsize
+
+# Decide which region we want to load
+load_region = [[0.49 * b, 0.51*b] for b in boxsize]
+mask.constrain_spatial(load_region)
+
+mask_size = np.asarray(mask).size
+mask_size = np.sum(mask.gas[:,1]) - np.sum(mask.gas[:,0])
+
+# Write the subset
+subset_writer = SWIFTWriterDatasubset(infile, outfile, mask, mask_size)
+subset_writer.write_datasubset()
+
+# Compare written subset of snapshot against corresponding region in full snapshot
+snapshot = sw.load(infile)
+sub_snapshot = sw.load(outfile)
+
+# First check the metadata
+compare(snapshot.metadata, sub_snapshot.metadata)
+
--- swiftsimio/subset_writer.py
+++ swiftsimio/subset_writer.py
@@ -35,26 +35,51 @@ class SWIFTDatasubset(object):
     def find_datasets(self, name, node):
         #if isinstance(node, h5py.Dataset) and not name in self.dataset_names:
         # ALEXEI: for testing
-        if isinstance(node, h5py.Dataset) and not name in self.dataset_names and name in self.test_dataset:
+        if isinstance(node, h5py.Dataset) and not name in self.dataset_names and "Cells" not in name:
             self.dataset_names.append(name)
+
+    def compute_mask_size(self, dataset_name):
+        legend = {"Cells": 0, 
+                  "PartType0": self.mask.gas_size, 
+                  "PartType1": self.mask.dark_matter_size, 
+                  "PartType2": 0, 
+                  "PartType3": 0, 
+                  "PartType4": self.mask.stars_size, 
+                  "PartType5": self.mask.black_holes_size}
+        for key in legend.keys():
+            if key in dataset_name:
+                return legend[key]
+
+    def get_mask_label(self, dataset_name):
+        legend = {"Cells": 0, 
+                  "PartType0": self.mask.gas, 
+                  "PartType1": self.mask.dark_matter, 
+                  "PartType2": 0, 
+                  "PartType3": 0, 
+                  "PartType4": self.mask.stars, 
+                  "PartType5": self.mask.black_holes}
+        for key in legend.keys():
+            if key in dataset_name:
+                return legend[key]
 
     def load_datasets(self):
         with h5py.File(self.filename, "r") as infile:
             if self.mask is not None:
-                # ALEXEI: note we restrict the range of dataset names for testing for now
-                #for name in self.dataset_names:
-                name = self.test_dataset
-                # get output dtype and size 
-                first_value = infile[name][0]
-                output_type = first_value.dtype
-                output_size = first_value.size
-                if output_size != 1:
-                    output_shape = (self.mask_size, output_size)
-                else:
-                    output_shape = self.mask_size
+                for name in self.dataset_names:
+                    # get output dtype and size 
+                    first_value = infile[name][0]
+                    output_type = first_value.dtype
+                    output_size = first_value.size
+                    #mask_size = np.sum(self.mask.gas[:,1] - self.mask.gas[:,0])
+                    mask_size = self.compute_mask_size(name)
+                    if output_size != 1:
+                        output_shape = (mask_size, output_size)
+                    else:
+                        output_shape = mask_size
 
-                print(self.mask)
-                self.datasets.append(read_ranges_from_file(infile[name], self.mask.gas, output_shape = output_shape, output_type = output_type))
+                    print(name, mask_size, output_shape, output_size, output_type)
+                    mask = self.get_mask_label(name)
+                    self.datasets.append(read_ranges_from_file(infile[name], mask, output_shape = output_shape, output_type = output_type))
 
 
 
@@ -88,7 +113,7 @@ class SWIFTWriterDatasubset(object):
         """
         Write the SWIFTDatasubset previously read in
         """
-        with h5py.File(self.output_filename, "w") as output_file:
-            print(self.subset.dataset_names, len(self.subset.datasets))
+        with h5py.File(self.output_filename, "a") as output_file:
+            #print(self.subset.dataset_names, len(self.subset.datasets))
             for i in range(len(self.subset.dataset_names)):
                 output_file.create_dataset(self.subset.dataset_names[i], data=self.subset.datasets[i])
