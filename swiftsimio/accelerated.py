a67d1c05af4727c0237cc1e2226d8fe1a3acf6b2
Alexei Borissov <dc-bori1@login7c.pri.cosma7.alces.network>
2020-05-19 15:29:27 +0100

    addressed Josh's minor issues

--- swiftsimio/accelerated.py
+++ swiftsimio/accelerated.py
@@ -121,13 +121,13 @@ def read_ranges_from_file(
     handle_multidim = handle.ndim > 1
 
     for (read_start, read_end) in ranges:
-        #t_0 = time.perf_counter()
+        # t_0 = time.perf_counter()
         if read_end == read_start:
             continue
 
         # Because we read inclusively
         size_of_range = read_end - read_start
-        
+
         # Construct selectors so we can use read_direct to prevent creating
         # copies of data from the hdf5 file.
         hdf5_read_sel = (
@@ -139,18 +139,18 @@ def read_ranges_from_file(
         output_dest_sel = np.s_[already_read : size_of_range + already_read]
 
         # Timers init
-        #t_start = time.perf_counter()
+        # t_start = time.perf_counter()
 
         handle.read_direct(output, source_sel=hdf5_read_sel, dest_sel=output_dest_sel)
 
         # Timers end
-        #t_end = time.perf_counter()
-        #data_write_size = size_of_range*output.itemsize
+        # t_end = time.perf_counter()
+        # data_write_size = size_of_range*output.itemsize
 
         already_read += size_of_range
-        
-        #t_f = time.perf_counter()
-        #print(size_of_range, data_write_size, t_end - t_start, data_write_size/(t_end - t_start), t_f - t_0)
+
+        # t_f = time.perf_counter()
+        # print(size_of_range, data_write_size, t_end - t_start, data_write_size/(t_end - t_start), t_f - t_0)
 
     return output
 
@@ -184,34 +184,30 @@ def index_dataset(handle: Dataset, mask_array: np.array) -> np.array:
 
 ################################ ALEXEI: playing around with better read_ranges_from_file implementation #################################
 
+
 def get_chunk_ranges(ranges, chunk_size, array_length):
     chunk_ranges = []
     for bound in ranges:
-        # ALEXEI: just checking Josh's implementation works here
-        #lower = int(np.floor(bound[0]/chunk_size))*chunk_size
-        #upper = min(int(np.ceil(bound[1]/chunk_size))*chunk_size, array_length)
         lower = (bound[0] // chunk_size) * chunk_size
-        upper = min(- ((- bound[1]) / chunk_size ) * chunk_size, array_length)
+        upper = min(-((-bound[1]) // chunk_size) * chunk_size, array_length)
 
-        # Before appending new chunk range we need to check 
-        # that it doesn't already exist or overlap with an 
+        # Before appending new chunk range we need to check
+        # that it doesn't already exist or overlap with an
         # existing one. The only way overlap can happen is
         # if the current lower index is less than or equal
-        # to the previous upper one. In that case simply 
+        # to the previous upper one. In that case simply
         # update the previous upper to cover current chunk
         if len(chunk_ranges) > 0:
             if lower > chunk_ranges[-1][1]:
                 chunk_ranges.append([lower, upper])
             elif lower <= chunk_ranges[-1][1]:
                 chunk_ranges[-1][1] = upper
-            # ALEXEI: remove for production. can be used for debugging
-            #else:
-            #    raise RuntimeError("computing chunk ranges has gone horribly wrong")
         # If chunk_ranges is empty, don't do any checks
         else:
             chunk_ranges.append([lower, upper])
 
     return chunk_ranges
+
 
 @jit(forceobj=True, fastmath=True)
 def expand_ranges(ranges):
@@ -223,10 +219,11 @@ def expand_ranges(ranges):
         lower = bounds[0]
         upper = bounds[1]
         bound_length = upper - lower
-        output[i:i+bound_length] = np.arange(lower, upper, dtype=np.int64)
+        output[i : i + bound_length] = np.arange(lower, upper, dtype=np.int64)
         i += bound_length
 
     return output
+
 
 def extract_ranges_from_chunks(array, chunks, ranges):
     # Find out which of the chunks in the chunks array each range in ranges belongs to
@@ -234,7 +231,10 @@ def extract_ranges_from_chunks(array, chunks, ranges):
     chunk_index = 0
     i = 0
     while i < len(ranges):
-        if chunks[chunk_index][0] <= ranges[i][0] and chunks[chunk_index][1] >= ranges[i][1]:
+        if (
+            chunks[chunk_index][0] <= ranges[i][0]
+            and chunks[chunk_index][1] >= ranges[i][1]
+        ):
             chunk_array_index[i] = chunk_index
             i += 1
         else:
@@ -250,12 +250,15 @@ def extract_ranges_from_chunks(array, chunks, ranges):
         adjusted_ranges[i][0] = ranges[i][0] - offset
         adjusted_ranges[i][1] = ranges[i][1] - offset
         try:
-            if chunk_array_index[i+1] > chunk_array_index[i]:
-                running_sum += chunks[chunk_array_index[i]][1] - chunks[chunk_array_index[i]][0]
+            if chunk_array_index[i + 1] > chunk_array_index[i]:
+                running_sum += (
+                    chunks[chunk_array_index[i]][1] - chunks[chunk_array_index[i]][0]
+                )
         except:
             pass
 
     return array[expand_ranges(adjusted_ranges)]
+
 
 def new_read_ranges_from_file(
     handle: Dataset,
@@ -299,27 +302,25 @@ def new_read_ranges_from_file(
 
     # Get chunk size
     chunk_ranges = get_chunk_ranges(ranges, handle.chunks[0], handle.size)
-    chunk_size = np.int32(np.sum([elem[1] - elem[0] for elem in chunk_ranges]))
+    chunk_size = np.sum([elem[1] - elem[0] for elem in chunk_ranges])
     shape = output_shape
     if isinstance(output_shape, tuple):
         shape[0] = chunk_size
     else:
         shape = chunk_size
 
-    print(shape, output_type)
     output = np.empty(shape, dtype=output_type)
     already_read = 0
     handle_multidim = handle.ndim > 1
-        
+
     for (read_start, read_end) in chunk_ranges:
-        #t_0 = time.perf_counter()
+        # t_0 = time.perf_counter()
         if read_end == read_start:
             continue
 
-
         # Because we read inclusively
         size_of_range = read_end - read_start
-        
+
         # Construct selectors so we can use read_direct to prevent creating
         # copies of data from the hdf5 file.
         hdf5_read_sel = (
@@ -333,5 +334,5 @@ def new_read_ranges_from_file(
         handle.read_direct(output, source_sel=hdf5_read_sel, dest_sel=output_dest_sel)
 
         already_read += size_of_range
-        
+
     return extract_ranges_from_chunks(output, chunk_ranges, ranges)
