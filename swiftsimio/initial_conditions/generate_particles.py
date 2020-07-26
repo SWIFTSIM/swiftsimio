408353e665b149980a71b1ffafe1c03207ea656c
mladenivkovic <mladen.ivkovic@hotmail.com>
2020-07-27 01:32:11 +0200

    added 3D case plotting

--- swiftsimio/initial_conditions/generate_particles.py
+++ swiftsimio/initial_conditions/generate_particles.py
@@ -576,15 +576,15 @@ def generate_IC_for_given_density(
     # use unitless arrays from this point on
     x_nounit = x.value
 
-    if icRunParams["PLOT_AT_REDISTRIBUTION"]:
-        # drop a first plot
-        tree = KDTree(x_nounit[:, :ndim], boxsize=boxsizeForTree)
-        for p in range(npart):
-            dist, neighs = tree.query(x_nounit[p, :ndim], k=Nngb_int)
-            h[p] = dist[-1] / kernel_gamma
-            W = kernel_func(dist, dist[-1])
-            rho[p] = (W * m[neighs]).sum()
-        _plot_current_situation(True, 0, x_nounit, rho, rho_anal, icSimParams)
+    #  if icRunParams["PLOT_AT_REDISTRIBUTION"]:
+    #      # drop a first plot
+    #      tree = KDTree(x_nounit[:, :ndim], boxsize=boxsizeForTree)
+    #      for p in range(npart):
+    #          dist, neighs = tree.query(x_nounit[p, :ndim], k=Nngb_int)
+    #          h[p] = dist[-1] / kernel_gamma
+    #          W = kernel_func(dist, dist[-1])
+    #          rho[p] = (W * m[neighs]).sum()
+    #      _plot_current_situation(True, 0, x_nounit, rho, rho_anal, icSimParams)
 
     # start iteration loop
     iteration = 0
@@ -941,43 +941,41 @@ def _plot_current_situation(
     + For debugging/checking purposes only, not meant to be called.
     """
     from matplotlib import pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
     from scipy import stats
 
     boxsize = icSimParams["boxsizeToUse"]
     ndim = icSimParams["ndim"]
+    nx = icSimParams["nx"]
 
     if x.shape[0] > 5000:
         marker = ","
     else:
         marker = "."
 
+    # dict to generate coordinates to compute analytical solution
+    tempparams = IC_set_IC_params(
+        boxsize=icSimParams["boxsize"],
+        periodic=icSimParams["periodic"],
+        nx=200,
+        ndim=1,
+        unit_l=icSimParams["unit_l"],
+    )
+
     if ndim == 1:
+
         fig = plt.figure(figsize=(6, 6), dpi=200)
         ax1 = fig.add_subplot(111)
         ax1.scatter(x[:, 0], rho, s=1, c="k", label="IC")
 
-        r = np.zeros((100, 3), dtype=np.float)
-        r[:, 0] = np.linspace(0, boxsize[0], 100)
-        ax1.plot(r[:, 0], rho_anal(r, ndim), label="analytical")
+        XA = IC_uniform_coordinates(tempparams)
+        ax1.plot(XA[:, 0], rho_anal(XA, ndim), label="analytical")
         ax1.set_xlabel("x")
         ax1.set_ylabel("rho")
         ax1.legend()
 
     elif ndim == 2:
-        fig = plt.figure(figsize=(18, 6), dpi=200)
 
-        # get rho map
-        #  X = np.linspace(0, 1, 20)
-        #  Y = np.linspace(0, 1, 20)
-        #  X, Y = np.meshgrid(X, Y)
-        #  xmap = np.stack((X.ravel(), Y.ravel()), axis=1)
-        #  rhomap = rho_anal(xmap).reshape(20,20)
-        #
-        #  ax1 = fig.add_subplot(1,1,1, projection='3d')
-        #  im1 = ax1.plot_surface(X, Y, rhomap, color='red', alpha=0.2)
-        #  ax1.scatter(x[:,0], x[:,1], rho, c='blue', depthshade=True, lw=0, s=2)
-        #  plt.show()
+        fig = plt.figure(figsize=(18, 6), dpi=200)
 
         ax1 = fig.add_subplot(131, aspect="equal")
         ax2 = fig.add_subplot(132,)
@@ -991,32 +989,34 @@ def _plot_current_situation(
 
         # x - rho plot
         rho_mean, edges, _ = stats.binned_statistic(
-            x[:, 0], rho, statistic="mean", bins=50
+            x[:, 0], rho, statistic="mean", bins=min(nx, 50)
         )
         rho2mean, edges, _ = stats.binned_statistic(
-            x[:, 0], rho ** 2, statistic="mean", bins=50
+            x[:, 0], rho ** 2, statistic="mean", bins=min(nx, 50)
         )
         rho_std = np.sqrt(rho2mean - rho_mean ** 2)
 
         r = 0.5 * (edges[:-1] + edges[1:])
         ax2.errorbar(r, rho_mean, yerr=rho_std, label="average all ptcls", lw=1)
 
-        xa = np.linspace(0, 1, 100)
-        ya = np.ones(xa.shape) * 0.5
-        XA = np.vstack((xa, ya)).T
-        ax2.plot(xa, rho_anal(XA, ndim), label="analytical, y = 0.5")
+        tempparams["boxsizeToUse"][0] = icSimParams["boxsizeToUse"][0]
+        XA = IC_uniform_coordinates(tempparams)
+        XA[:, 1] = 0.5 * boxsize[1]
+        ax2.plot(XA[:, 0], rho_anal(XA, ndim), label="analytical, y = 0.5")
 
         ax2.scatter(
             x[:, 0], rho, s=1, alpha=0.4, c="k", label="all particles", marker=","
         )
-        selection = np.logical_and(x[:, 1] > 0.45, x[:, 1] < 0.55)
+        selection = np.logical_and(
+            x[:, 1] > 0.45 * boxsize[1], x[:, 1] < 0.55 * boxsize[1]
+        )
         ax2.scatter(
             x[selection, 0],
             rho[selection],
             s=2,
             alpha=0.8,
             c="r",
-            label="0.45 < y < 0.55",
+            label="0.45 boxsize < y < 0.55 boxsize",
         )
 
         ax2.legend()
@@ -1025,25 +1025,29 @@ def _plot_current_situation(
 
         # y - rho plot
         rho_mean, edges, _ = stats.binned_statistic(
-            x[:, 1], rho, statistic="mean", bins=50
+            x[:, 1], rho, statistic="mean", bins=min(nx, 50)
         )
         rho2mean, edges, _ = stats.binned_statistic(
-            x[:, 1], rho ** 2, statistic="mean", bins=50
+            x[:, 1], rho ** 2, statistic="mean", bins=min(nx, 50)
         )
         rho_std = np.sqrt(rho2mean - rho_mean ** 2)
 
         r = 0.5 * (edges[:-1] + edges[1:])
         ax3.errorbar(r, rho_mean, yerr=rho_std, label="average all ptcls", lw=1)
 
-        ya = np.linspace(0, 1, 100)
-        xa = np.ones(ya.shape) * 0.5
-        XA = np.vstack((xa, ya)).T
-        ax3.plot(ya, rho_anal(XA, ndim), label="analytical x = 0.5")
+        tempparams["boxsizeToUse"][0] = icSimParams["boxsizeToUse"][1]
+        XA = IC_uniform_coordinates(tempparams)
+        XA[:, 1] = XA[:, 0]
+        XA[:, 0] = 0.5 * boxsize[0]
+        xa = XA[:, 0]
+        ax3.plot(XA[:, 1], rho_anal(XA, ndim), label="analytical x = 0.5")
 
         ax3.scatter(
             x[:, 1], rho, s=1, alpha=0.4, c="k", label="all particles", marker=","
         )
-        selection = np.logical_and(x[:, 0] > 0.45, x[:, 0] < 0.55)
+        selection = np.logical_and(
+            x[:, 0] > 0.45 * boxsize[0], x[:, 0] < 0.55 * boxsize[0]
+        )
         ax3.scatter(
             x[selection, 1],
             rho[selection],
@@ -1056,6 +1060,111 @@ def _plot_current_situation(
         ax3.legend()
         ax3.set_xlabel("y")
         ax3.set_ylabel("rho")
+
+    elif ndim == 3:
+
+        fig = plt.figure(figsize=(15, 10), dpi=200)
+
+        ax1 = fig.add_subplot(231, aspect="equal")
+        ax2 = fig.add_subplot(232, aspect="equal")
+        ax3 = fig.add_subplot(233, aspect="equal")
+        ax4 = fig.add_subplot(234,)
+        ax5 = fig.add_subplot(235,)
+        ax6 = fig.add_subplot(236,)
+
+        # coordinate scatterplots
+
+        ax1.scatter(x[:, 0], x[:, 1], s=1, alpha=0.5, c="k", marker=marker)
+        ax1.set_xlabel("x")
+        ax1.set_ylabel("y")
+
+        ax2.scatter(x[:, 1], x[:, 2], s=1, alpha=0.5, c="k", marker=marker)
+        ax2.set_xlabel("y")
+        ax2.set_ylabel("z")
+
+        ax3.scatter(x[:, 2], x[:, 0], s=1, alpha=0.5, c="k", marker=marker)
+        ax3.set_xlabel("z")
+        ax3.set_ylabel("x")
+
+        # x - rho plot
+        rho_mean, edges, _ = stats.binned_statistic(
+            x[:, 0], rho, statistic="mean", bins=min(nx, 50)
+        )
+        rho2mean, edges, _ = stats.binned_statistic(
+            x[:, 0], rho ** 2, statistic="mean", bins=min(nx, 50)
+        )
+        rho_std = np.sqrt(rho2mean - rho_mean ** 2)
+
+        r = 0.5 * (edges[:-1] + edges[1:])
+        ax4.errorbar(r, rho_mean, yerr=rho_std, label="average all ptcls", lw=1)
+
+        tempparams["boxsizeToUse"][0] = icSimParams["boxsizeToUse"][0]
+        XA = IC_uniform_coordinates(tempparams)
+        XA[:, 1] = 0.5 * boxsize[1]
+        XA[:, 2] = 0.5 * boxsize[2]
+        ax4.plot(XA[:, 0], rho_anal(XA, ndim), label="analytical, y, z = 1/2 boxsize")
+
+        ax4.scatter(
+            x[:, 0], rho, s=1, alpha=0.4, c="k", label="all particles", marker=","
+        )
+
+        ax4.legend()
+        ax4.set_xlabel("x")
+        ax4.set_ylabel("rho")
+
+        # y - rho plot
+        rho_mean, edges, _ = stats.binned_statistic(
+            x[:, 1], rho, statistic="mean", bins=min(50, nx)
+        )
+        rho2mean, edges, _ = stats.binned_statistic(
+            x[:, 1], rho ** 2, statistic="mean", bins=min(50, nx)
+        )
+        rho_std = np.sqrt(rho2mean - rho_mean ** 2)
+
+        r = 0.5 * (edges[:-1] + edges[1:])
+        ax5.errorbar(r, rho_mean, yerr=rho_std, label="average all ptcls", lw=1)
+
+        tempparams["boxsizeToUse"][0] = icSimParams["boxsizeToUse"][1]
+        XA = IC_uniform_coordinates(tempparams)
+        XA[:, 1] = XA[:, 0]
+        XA[:, 0] = 0.5 * boxsize[0]
+        XA[:, 2] = 0.5 * boxsize[2]
+        ax5.plot(XA[:, 1], rho_anal(XA, ndim), label="analytical, x, z = 1/2 boxsize")
+
+        ax5.scatter(
+            x[:, 1], rho, s=1, alpha=0.4, c="k", label="all particles", marker=","
+        )
+
+        ax5.legend()
+        ax5.set_xlabel("y")
+        ax5.set_ylabel("rho")
+
+        # z - rho plot
+        rho_mean, edges, _ = stats.binned_statistic(
+            x[:, 2], rho, statistic="mean", bins=min(50, nx)
+        )
+        rho2mean, edges, _ = stats.binned_statistic(
+            x[:, 2], rho ** 2, statistic="mean", bins=min(50, nx)
+        )
+        rho_std = np.sqrt(rho2mean - rho_mean ** 2)
+
+        r = 0.5 * (edges[:-1] + edges[1:])
+        ax6.errorbar(r, rho_mean, yerr=rho_std, label="average all ptcls", lw=1)
+
+        tempparams["boxsizeToUse"][0] = icSimParams["boxsizeToUse"][2]
+        XA = IC_uniform_coordinates(tempparams)
+        XA[:, 2] = XA[:, 0]
+        XA[:, 0] = 0.5 * boxsize[0]
+        XA[:, 1] = 0.5 * boxsize[2]
+        ax6.plot(XA[:, 2], rho_anal(XA, ndim), label="analytical, x, y = 1/2 boxsize")
+
+        ax6.scatter(
+            x[:, 2], rho, s=1, alpha=0.4, c="k", label="all particles", marker=","
+        )
+
+        ax6.legend()
+        ax6.set_xlabel("z")
+        ax6.set_ylabel("rho")
 
     ax1.set_title("Iteration =" + str(iteration))
     if save:
