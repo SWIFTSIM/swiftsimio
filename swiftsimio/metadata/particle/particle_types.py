316cdbdbf75618e9c7db16e378173b06426482d9
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-06-07 23:42:28 +0200

    Use the correct default particle type name for type 3 (sinks) and add the neutrinos (type 6) to the list.

--- swiftsimio/metadata/particle/particle_types.py
+++ swiftsimio/metadata/particle/particle_types.py
@@ -7,25 +7,28 @@ particle_name_underscores = {
     0: "gas",
     1: "dark_matter",
     2: "boundary",
-    3: "second_boundary",
+    3: "sinks",
     4: "stars",
     5: "black_holes",
+    6: "neutrinos",
 }
 
 particle_name_class = {
     0: "Gas",
     1: "DarkMatter",
     2: "Boundary",
-    3: "SecondBoundary",
+    3: "Sinks",
     4: "Stars",
     5: "BlackHoles",
+    6: "Neutrinos",
 }
 
 particle_name_text = {
     0: "Gas",
     1: "Dark Matter",
     2: "Boundary",
-    3: "Second Boundary",
+    3: "Sinks",
     4: "Stars",
     5: "Black Holes",
+    6: "Neutrinos",
 }
