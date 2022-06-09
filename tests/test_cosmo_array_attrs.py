5cdb4d5c21c496b174abdeddc963680acbfc48f8
Kyle Oman <kyle.a.oman@durham.ac.uk>
2022-06-09 16:56:37 +0100

    Add tests for cosmo_array ufuncs.

--- tests/test_cosmo_array_attrs.py
+++ tests/test_cosmo_array_attrs.py
@@ -5,6 +5,7 @@ Tests that functions returning copies of cosmo_array
 
 import pytest
 import numpy as np
+import unyt as u
 from swiftsimio.objects import cosmo_array, cosmo_factor, a
 
 
@@ -163,3 +164,353 @@ class TestCopyFuncs:
         )
         assert arr.compatible_with_comoving()
         assert arr.compatible_with_physical()
+
+class TestCosmoArrayUfuncs:
+    def test_preserving_ufunc(self):
+        # 1 argument
+        inp = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.ones_like(inp)
+        assert res.to_value(u.kpc) == 1
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor
+        # 2 argument, no cosmo_factors
+        inp = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        res = inp + inp
+        assert res.to_value(u.kpc) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor is None
+        # 2 argument, one is not cosmo_array
+        inp1 = u.unyt_array([2], u.kpc)
+        inp2 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        res = inp1 + inp2
+        assert res.to_value(u.kpc) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor is None
+        # 2 argument, two is not cosmo_array
+        inp1 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        inp2 = u.unyt_array([2], u.kpc)
+        res = inp1 + inp2
+        assert res.to_value(u.kpc) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor is None
+        # 2 argument, only one has cosmo_factor
+        inp1 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        inp2 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        with pytest.warns(RuntimeWarning, match="Mixing ufunc arguments"):
+            res = inp1 + inp2
+        assert res.to_value(u.kpc) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor == inp1.cosmo_factor
+        # 2 argument, only two has cosmo_factor
+        inp1 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        inp2 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        with pytest.warns(RuntimeWarning, match="Mixing ufunc arguments"):
+            res = inp1 + inp2
+        assert res.to_value(u.kpc) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor == inp2.cosmo_factor
+        # 2 argument, mismatched cosmo_factors
+        inp1 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        inp2 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=0.5),
+        )
+        with pytest.raises(
+            ValueError, match="Ufunc arguments have cosmo_factors that differ"
+        ):
+            res = inp1 + inp2
+        # 2 argument, matched cosmo_factors
+        inp = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = inp + inp
+        assert res.to_value(u.kpc) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor
+
+    def test_multiplying_ufunc(self):
+        # no cosmo_factors
+        inp = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        res = inp * inp
+        assert res.to_value(u.kpc**2) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor is None
+        # one is not cosmo_array
+        inp1 = 2
+        inp2 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        res = inp1 * inp2
+        assert res.to_value(u.kpc) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor == inp2.cosmo_factor
+        # two is not cosmo_array
+        inp1 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        inp2 = 2
+        res = inp1 * inp2
+        assert res.to_value(u.kpc) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor == inp1.cosmo_factor
+        # only one has cosmo_factor
+        inp1 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        inp2 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        with pytest.warns(RuntimeWarning, match="Mixing ufunc arguments"):
+            res = inp1 * inp2
+        assert res.to_value(u.kpc**2) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor is None
+        # only two has cosmo_factor
+        inp1 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        inp2 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        with pytest.warns(RuntimeWarning, match="Mixing ufunc arguments"):
+            res = inp1 * inp2
+        assert res.to_value(u.kpc**2) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor is None
+        # cosmo_factors both present
+        inp = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = inp * inp
+        assert res.to_value(u.kpc**2) == 4
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor**2
+
+    def test_dividing_ufunc(self):
+        inp = cosmo_array(
+            [2.0],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = inp / inp
+        assert res.to_value(u.dimensionless) == 1  # also ensures units ok
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor**0
+
+    def test_return_without_ufunc(self):
+        # 1 argument
+        inp = cosmo_array(
+            [1],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.logical_not(inp)
+        assert res == np.logical_not(1)
+        assert isinstance(res, np.ndarray) and not isinstance(res, u.unyt_array)
+        # 2 arguments, both cosmo_array
+        inp = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.logaddexp(inp, inp)
+        assert res == np.logaddexp(2, 2)
+        assert isinstance(res, np.ndarray) and not isinstance(res, u.unyt_array)
+        # 2 arguments, mismatched cosmo_factor
+        inp1 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        inp2 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=0.5),
+        )
+        with pytest.raises(
+            ValueError, match="Ufunc arguments have cosmo_factors that differ"
+        ):
+            res = np.logaddexp(inp1, inp2)
+        # 2 arguments, one missing comso_factor
+        inp1 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        inp2 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        with pytest.warns(RuntimeWarning, match="Mixing ufunc arguments"):
+            res = np.logaddexp(inp1, inp2)
+        assert res == np.logaddexp(2, 2)
+        assert isinstance(res, np.ndarray) and not isinstance(res, u.unyt_array)
+        # 2 arguments, two missing comso_factor
+        inp1 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        inp2 = cosmo_array([2], u.kpc, comoving=False, cosmo_factor=None)
+        with pytest.warns(RuntimeWarning, match="Mixing ufunc arguments"):
+            res = np.logaddexp(inp1, inp2)
+        assert res == np.logaddexp(2, 2)
+        assert isinstance(res, np.ndarray) and not isinstance(res, u.unyt_array)
+        # 2 arguments, one not cosmo_array
+        inp1 = u.unyt_array([2], u.kpc)
+        inp2 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        with pytest.warns(RuntimeWarning, match="Mixing ufunc arguments"):
+            res = np.logaddexp(inp1, inp2)
+        assert res == np.logaddexp(2, 2)
+        assert isinstance(res, np.ndarray) and not isinstance(res, u.unyt_array)
+        # 2 arguments, two not cosmo_array
+        inp1 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        inp2 = u.unyt_array([2], u.kpc)
+        with pytest.warns(RuntimeWarning, match="Mixing ufunc arguments"):
+            res = np.logaddexp(inp1, inp2)
+        assert res == np.logaddexp(2, 2)
+        assert isinstance(res, np.ndarray) and not isinstance(res, u.unyt_array)
+
+    def test_sqrt_ufunc(self):
+        inp = cosmo_array(
+            [4],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.sqrt(inp)
+        assert res.to_value(u.kpc**0.5) == 2  # also ensures units ok
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor**0.5
+
+    def test_square_ufunc(self):
+        inp = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.square(inp)
+        assert res.to_value(u.kpc**2) == 4  # also ensures units ok
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor**2
+
+    def test_reciprocal_ufunc(self):
+        inp = cosmo_array(
+            [2.0],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.reciprocal(inp)
+        assert res.to_value(u.kpc**-1) == 0.5  # also ensures units ok
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor**-1
+
+    def test_passthrough_ufunc(self):
+        # 1 argument
+        inp = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.negative(inp)
+        assert res.to_value(u.kpc) == -2
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor
+        # 2 argument, matching
+        inp = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.copysign(inp, inp)
+        assert res.to_value(u.kpc) == inp.to_value(u.kpc)
+        assert res.comoving is False
+        assert res.cosmo_factor == inp.cosmo_factor
+        # 2 argument, not matching
+        inp1 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        inp2 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=0.5),
+        )
+        with pytest.raises(ValueError):
+            res = np.copysign(inp1, inp2)
+
+    def test_arctan2_ufunc(self):
+        inp = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = np.arctan2(inp, inp)
+        assert res.to_value(u.dimensionless) == np.arctan2(2, 2)
+        assert res.comoving is False
+        assert res.cosmo_factor.a_factor == 1  # also ensures cosmo_factor present
+
+    def test_comparison_ufunc(self):
+        inp1 = cosmo_array(
+            [1],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        inp2 = cosmo_array(
+            [2],
+            u.kpc,
+            comoving=False,
+            cosmo_factor=cosmo_factor(a**1, scale_factor=1.0),
+        )
+        res = inp1 < inp2
+        assert res.all()
+        assert isinstance(res, np.ndarray) and not isinstance(res, u.unyt_array)
