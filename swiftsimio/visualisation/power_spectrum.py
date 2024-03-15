Fixed units

swiftsimio/visualisation/power_spectrum.py
@@ -10,6 +10,7 @@ from swiftsimio.accelerated import jit, NUM_THREADS, prange
 from swiftsimio import cosmo_array
 from swiftsimio.reader import __SWIFTParticleDataset
 
+
 @jit(nopython=True, fastmath=True)
 def deposit(
     x: float64,
@@ -208,13 +209,12 @@ def render_to_deposit(
                     f'Comoving quantity "{project}" is not compatible with physical coordinates!'
                 )
 
-
     # Get the box size
     box_size = data.metadata.boxsize
 
     if not box_size.units == positions.units:
         raise AttributeError("Box size and positions have different units!")
-    
+
     # Deposit the particles
     arguments = dict(
         x=positions[:, 0].v,
@@ -227,7 +227,7 @@ def render_to_deposit(
         box_y=box_size[1].v,
         box_z=box_size[2].v,
     )
-    
+
     if parallel:
         deposition = deposit_parallel(**arguments)
     else:
@@ -236,17 +236,22 @@ def render_to_deposit(
     comoving = positions.comoving
     coord_cosmo_factor = positions.cosmo_factor
 
-    units = 1.0 / (data.metadata.boxsize[0] * data.metadata.boxsize[1] * data.metadata.boxsize[2])
+    units = 1.0 / (
+        data.metadata.boxsize[0] * data.metadata.boxsize[1] * data.metadata.boxsize[2]
+    )
     units.convert_to_units(1.0 / data.metadata.boxsize.units ** 3)
 
     units *= quantity.units
     new_cosmo_factor = quantity.cosmo_factor / (coord_cosmo_factor ** 3)
 
-    return cosmo_array(deposition, comoving=comoving, cosmo_factor=new_cosmo_factor, units=units)
-    
+    return cosmo_array(
+        deposition, comoving=comoving, cosmo_factor=new_cosmo_factor, units=units
+    )
 
 
-def deposition_to_power_spectrum(deposition: ndarray, box_size: cosmo_array, folding: float = 1.0) -> ndarray:
+def deposition_to_power_spectrum(
+    deposition: ndarray, box_size: cosmo_array, folding: float = 1.0
+) -> ndarray:
     """
     Convert a deposition to a power spectrum.
 
@@ -300,17 +305,28 @@ def deposition_to_power_spectrum(deposition: ndarray, box_size: cosmo_array, fol
     # fourier_amplitudes = fourier_amplitudes.flatten()
 
     kbins = np.linspace(kfreq.min(), kfreq.max(), npix // 2 + 1)
-    #kbins = np.arange(0.5, npix//2+1, 1.)
+    # kbins = np.arange(0.5, npix//2+1, 1.)
     # kvals are in pixel co-ordinates. We know the pixels
     # span the entire box, so we can convert to physical
     # co-ordinates.
     kvals = 0.5 * (kbins[1:] + kbins[:-1])
 
-    power_spectrum = (
-        np.histogram(knrm, bins=kbins, weights=fourier_amplitudes)[0]
-        / np.histogram(knrm, bins=kbins)[0]
+    binned_amplitudes = np.histogram(knrm, bins=kbins, weights=fourier_amplitudes.v)[0]
+    binned_counts = np.histogram(knrm, bins=kbins)[0]
+
+    zero_mask = binned_counts == 0
+
+    # Avoid divide by zero
+    binned_amplitudes[zero_mask] = 0.0
+    binned_counts[zero_mask] = 1.0
+
+    # Correct for folding
+    binned_amplitudes *= folding ** 3
+
+    # Correct units and names
+    kvals.name = "k"
+    power_spectrum = unyt.unyt_array(
+        binned_amplitudes / binned_counts, units=fourier_amplitudes.units, name="P(k)"
     )
-    
-    power_spectrum *= folding ** 3
 
     return kvals, power_spectrum

tests/test_visualisation.py
@@ -25,6 +25,7 @@ from swiftsimio.visualisation.smoothing_length_generation import (
 from swiftsimio.optional_packages import CudaSupportError, CUDA_AVAILABLE
 from swiftsimio.objects import cosmo_array, a
 from unyt.array import unyt_array
+import unyt
 
 from tests.helper import requires
 
@@ -78,7 +79,7 @@ def test_scatter_mass_conservation():
 
     for resolution in resolutions:
         image = scatter(x, y, m, h, resolution, 1.0, 1.0)
-        mass_in_image = image.sum() / (resolution**2)
+        mass_in_image = image.sum() / (resolution ** 2)
 
         # Check mass conservation to 5%
         assert np.isclose(mass_in_image, total_mass, 0.05)
@@ -333,7 +334,7 @@ def test_comoving_versus_physical(filename):
         # conversion in this case
         img = func(data, resolution=256, project=None)
         assert img.comoving
-        assert img.cosmo_factor.expr == a**aexp
+        assert img.cosmo_factor.expr == a ** aexp
         img = func(data, resolution=256, project="densities")
         assert img.comoving
         assert img.cosmo_factor.expr == a ** (aexp - 3.0)
@@ -351,7 +352,7 @@ def test_comoving_versus_physical(filename):
         img = func(data, resolution=256, project="masses")
         # check that we get a physical result
         assert not img.comoving
-        assert img.cosmo_factor.expr == a**aexp
+        assert img.cosmo_factor.expr == a ** aexp
         # densities are still compatible with physical
         img = func(data, resolution=256, project="densities")
         assert not img.comoving
@@ -529,7 +530,7 @@ def test_volume_render_and_unfolded_deposit():
         boxsize,
         boxsize,
         boxsize,
-    ) / (res**3)
+    ) / (res ** 3)
 
     assert np.allclose(deposition, volume)
 
@@ -571,16 +572,18 @@ def test_volume_render_and_unfolded_deposit_with_units(filename):
     # Volume render the particles
     volume = render_gas(data, npix, parallel=False).to_physical()
 
-    mean_density_deposit = (np.sum(deposition) / npix**3).to("Msun / kpc**3").v
-    mean_density_volume = (np.sum(volume) / npix**3).to("Msun / kpc**3").v
-    mean_density_calculated = (np.sum(data.gas.masses) / (data.metadata.boxsize[0] * data.metadata.a)**3).to("Msun / kpc**3").v
+    mean_density_deposit = (np.sum(deposition) / npix ** 3).to("Msun / kpc**3").v
+    mean_density_volume = (np.sum(volume) / npix ** 3).to("Msun / kpc**3").v
+    mean_density_calculated = (
+        (np.sum(data.gas.masses) / (data.metadata.boxsize[0] * data.metadata.a) ** 3)
+        .to("Msun / kpc**3")
+        .v
+    )
 
     assert np.isclose(mean_density_deposit, mean_density_calculated)
     assert np.isclose(mean_density_volume, mean_density_calculated, rtol=0.2)
 
-    assert np.isclose(
-        mean_density_deposit, mean_density_volume, rtol=0.2
-    )
+    assert np.isclose(mean_density_deposit, mean_density_volume, rtol=0.2)
 
     # assert np.isclose(deposition.to(volume.units)[0, 0, 0].v, volume[0, 0, 0].v, rtol=0.2)
 
@@ -613,7 +616,7 @@ def test_dark_matter_power_spectrum(filename, save=True):
 
     folding_output = {}
 
-    for folding in [8.0, 64.0]:#, 8.0, 512.0]:
+    for folding in [8.0, 64.0]:  # , 8.0, 512.0]:
         # Deposit the particles
         deposition = render_to_deposit(
             data.dark_matter, 128, project="masses", folding=folding, parallel=False
@@ -629,18 +632,32 @@ def test_dark_matter_power_spectrum(filename, save=True):
     if save:
         import matplotlib.pyplot as plt
 
-        for npix, (k, power_spectrum) in output.items():
-            plt.plot(k, power_spectrum, label=f"Npix {npix}")
+        with unyt.matplotlib_support:
+            for npix, (k, power_spectrum) in output.items():
+                plt.plot(k, power_spectrum, label=f"Npix {npix}")
 
-        for fold_id, (k, power_spectrum) in folding_output.items():
-            plt.plot(k, power_spectrum, label=f"Fold {fold_id} (Npix 128)", ls='dotted')
-        plt.xlabel("k")
-        plt.loglog()
-        plt.axvline(2 * np.pi / data.metadata.boxsize[0].to("Mpc"), color="black", linestyle="--")
-        plt.axvline(2 * np.pi / (data.metadata.boxsize[0].to("Mpc") / len(data.dark_matter.smoothing_lengths)**(1/3)), color="black", linestyle="--")
-        plt.ylabel("P(k)")
-        plt.text(0.05, 0.05, data.metadata.boxsize, transform=plt.gca().transAxes)
-        plt.legend()
-        plt.savefig("dark_matter_power_spectrum.png")
+            for fold_id, (k, power_spectrum) in folding_output.items():
+                plt.plot(
+                    k, power_spectrum, label=f"Fold {fold_id} (Npix 128)", ls="dotted"
+                )
+            plt.loglog()
+            plt.axvline(
+                2 * np.pi / data.metadata.boxsize[0].to("Mpc"),
+                color="black",
+                linestyle="--",
+            )
+            plt.axvline(
+                2
+                * np.pi
+                / (
+                    data.metadata.boxsize[0].to("Mpc")
+                    / len(data.dark_matter.smoothing_lengths) ** (1 / 3)
+                ),
+                color="black",
+                linestyle="--",
+            )
+            plt.text(0.05, 0.05, data.metadata.boxsize, transform=plt.gca().transAxes)
+            plt.legend()
+            plt.savefig("dark_matter_power_spectrum.png")
 
     return