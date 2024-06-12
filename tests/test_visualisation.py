da12cf8cec92ce19e3413dc3028ef52afb81c8b8
Josh Borrow <josh@joshborrow.com>
2024-06-12 10:39:50 -0400

    Format

--- tests/test_visualisation.py
+++ tests/test_visualisation.py
@@ -88,7 +88,7 @@ def test_scatter_mass_conservation():
 
     for resolution in resolutions:
         image = scatter(x, y, m, h, resolution, 1.0, 1.0)
-        mass_in_image = image.sum() / (resolution**2)
+        mass_in_image = image.sum() / (resolution ** 2)
 
         # Check mass conservation to 5%
         assert np.isclose(mass_in_image, total_mass, 0.05)
@@ -346,7 +346,7 @@ def test_comoving_versus_physical(filename):
         # conversion in this case
         img = func(data, resolution=256, project=None)
         assert img.comoving
-        assert img.cosmo_factor.expr == a**aexp
+        assert img.cosmo_factor.expr == a ** aexp
         img = func(data, resolution=256, project="densities")
         assert img.comoving
         assert img.cosmo_factor.expr == a ** (aexp - 3.0)
@@ -364,7 +364,7 @@ def test_comoving_versus_physical(filename):
         img = func(data, resolution=256, project="masses")
         # check that we get a physical result
         assert not img.comoving
-        assert img.cosmo_factor.expr == a**aexp
+        assert img.cosmo_factor.expr == a ** aexp
         # densities are still compatible with physical
         img = func(data, resolution=256, project="densities")
         assert not img.comoving
@@ -587,8 +587,8 @@ def test_volume_render_and_unfolded_deposit_with_units(filename):
     # Volume render the particles
     volume = render_gas(data, npix, parallel=False).to_physical()
 
-    mean_density_deposit = (np.sum(deposition) / npix**3).to("Msun / kpc**3").v
-    mean_density_volume = (np.sum(volume) / npix**3).to("Msun / kpc**3").v
+    mean_density_deposit = (np.sum(deposition) / npix ** 3).to("Msun / kpc**3").v
+    mean_density_volume = (np.sum(volume) / npix ** 3).to("Msun / kpc**3").v
     mean_density_calculated = (
         (np.sum(data.gas.masses) / (data.metadata.boxsize[0] * data.metadata.a) ** 3)
         .to("Msun / kpc**3")
@@ -615,8 +615,7 @@ def test_dark_matter_power_spectrum(filename, save=False):
     max_k = 1e2 / unyt.Mpc
 
     bins = unyt.unyt_array(
-        np.logspace(np.log10(min_k.v), np.log10(max_k.v), 32),
-        units=min_k.units,
+        np.logspace(np.log10(min_k.v), np.log10(max_k.v), 32), units=min_k.units
     )
 
     output = {}
@@ -651,7 +650,7 @@ def test_dark_matter_power_spectrum(filename, save=False):
 
         folds[folding] = deposition
 
-        folding_output[2**folding] = (k, power_spectrum, scatter)
+        folding_output[2 ** folding] = (k, power_spectrum, scatter)
 
     # Now try doing them all together at once.
 
@@ -684,11 +683,7 @@ def test_dark_matter_power_spectrum(filename, save=False):
             norm = LogNorm()
             colors = cmap(norm(folding_tracker))
             plt.scatter(
-                all_centers,
-                all_ps,
-                label="Full Fold",
-                color=colors,
-                edgecolor="pink",
+                all_centers, all_ps, label="Full Fold", color=colors, edgecolor="pink"
             )
             plt.colorbar(
                 mappable=ScalarMappable(norm=norm, cmap=cmap),
