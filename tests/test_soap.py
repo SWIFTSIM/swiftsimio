6b26291641e3300005039513e2887968fc18f467
Josh Borrow <josh@joshborrow.com>
2024-09-18 17:03:58 +0200

    Added test loading a SOAP catalogue

---
+++ tests/test_soap.py
@@ -0,0 +1,13 @@
+"""
+Tests that we can open SOAP files
+"""
+
+from tests.helper import requires
+
+from swiftsimio import load
+
+@requires("soap_example.hdf5")
+def test_soap_can_load(filename):
+    data = load(filename)
+
+    return
