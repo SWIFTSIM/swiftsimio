1beeacb8af264c9cf0adc2dd2a5070b58545d993
Kyle Oman <kyle.a.oman@durham.ac.uk>
2024-12-05 23:05:15 +0800

    Add test cases for copying cosmo_array.

--- tests/test_cosmo_array.py
+++ tests/test_cosmo_array.py
@@ -5,6 +5,7 @@ Tests the initialisation of a cosmo_array.
 import numpy as np
 import unyt as u
 from swiftsimio.objects import cosmo_array, cosmo_factor
+from copy import copy, deepcopy
 
 
 class TestCosmoArrayInit:
@@ -46,3 +47,37 @@ class TestCosmoArrayInit:
         assert hasattr(arr, "cosmo_factor")
         assert hasattr(arr, "comoving")
         assert isinstance(arr, cosmo_array)
+
+
+class TestCosmoArrayCopy:
+    def test_copy(self):
+        """
+        Check that when we copy a cosmo_array it preserves its values and attributes.
+        """
+        units = u.Mpc
+        arr = cosmo_array(
+            u.unyt_array(np.ones(5), units=units),
+            cosmo_factor=cosmo_factor("a^1", 1),
+            comoving=False,
+        )
+        copy_arr = copy(arr)
+        assert np.allclose(arr.to_value(units), copy_arr.to_value(units))
+        assert arr.units == copy_arr.units
+        assert arr.cosmo_factor == copy_arr.cosmo_factor
+        assert arr.comoving == copy_arr.comoving
+
+    def test_deepcopy(self):
+        """
+        Check that when we deepcopy a cosmo_array it preserves its values and attributes
+        """
+        units = u.Mpc
+        arr = cosmo_array(
+            u.unyt_array(np.ones(5), units=units),
+            cosmo_factor=cosmo_factor("a^1", 1),
+            comoving=False,
+        )
+        copy_arr = deepcopy(arr)
+        assert np.allclose(arr.to_value(units), copy_arr.to_value(units))
+        assert arr.units == copy_arr.units
+        assert arr.cosmo_factor == copy_arr.cosmo_factor
+        assert arr.comoving == copy_arr.comoving
