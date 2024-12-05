88c3739a00ad82be0ae6353573a47b90bd41a97f
Kyle Oman <kyle.a.oman@durham.ac.uk>
2024-12-05 23:13:57 +0800

    Run black.

--- tests/test_cosmo_array.py
+++ tests/test_cosmo_array.py
@@ -11,7 +11,10 @@ from copy import copy, deepcopy
 class TestCosmoArrayInit:
     def test_init_from_ndarray(self):
         arr = cosmo_array(
-            np.ones(5), units=u.Mpc, cosmo_factor=cosmo_factor(a**1, 1), comoving=False
+            np.ones(5),
+            units=u.Mpc,
+            cosmo_factor=cosmo_factor(a ** 1, 1),
+            comoving=False,
         )
         assert hasattr(arr, "cosmo_factor")
         assert hasattr(arr, "comoving")
@@ -21,7 +24,7 @@ class TestCosmoArrayInit:
         arr = cosmo_array(
             [1, 1, 1, 1, 1],
             units=u.Mpc,
-            cosmo_factor=cosmo_factor(a**1, 1),
+            cosmo_factor=cosmo_factor(a ** 1, 1),
             comoving=False,
         )
         assert hasattr(arr, "cosmo_factor")
@@ -31,7 +34,7 @@ class TestCosmoArrayInit:
     def test_init_from_unyt_array(self):
         arr = cosmo_array(
             u.unyt_array(np.ones(5), units=u.Mpc),
-            cosmo_factor=cosmo_factor(a**1, 1),
+            cosmo_factor=cosmo_factor(a ** 1, 1),
             comoving=False,
         )
         assert hasattr(arr, "cosmo_factor")
@@ -41,7 +44,7 @@ class TestCosmoArrayInit:
     def test_init_from_list_of_unyt_arrays(self):
         arr = cosmo_array(
             [u.unyt_array(1, units=u.Mpc) for _ in range(5)],
-            cosmo_factor=cosmo_factor(a**1, 1),
+            cosmo_factor=cosmo_factor(a ** 1, 1),
             comoving=False,
         )
         assert hasattr(arr, "cosmo_factor")
@@ -57,7 +60,7 @@ class TestCosmoArrayCopy:
         units = u.Mpc
         arr = cosmo_array(
             u.unyt_array(np.ones(5), units=units),
-            cosmo_factor=cosmo_factor(a**1, 1),
+            cosmo_factor=cosmo_factor(a ** 1, 1),
             comoving=False,
         )
         copy_arr = copy(arr)
@@ -73,7 +76,7 @@ class TestCosmoArrayCopy:
         units = u.Mpc
         arr = cosmo_array(
             u.unyt_array(np.ones(5), units=units),
-            cosmo_factor=cosmo_factor(a**1, 1),
+            cosmo_factor=cosmo_factor(a ** 1, 1),
             comoving=False,
         )
         copy_arr = deepcopy(arr)
