d784b29b2f435a2ff19334978ea376ea45636e99
Kyle Oman <kyle.a.oman@durham.ac.uk>
2025-02-13 09:19:19 +0000

    Update tests with norm arg removed from backend functions.

--- tests/test_visualisation.py
+++ tests/test_visualisation.py
@@ -57,7 +57,6 @@ def test_scatter(save=False):
                 res=256,
                 box_x=1.0,
                 box_y=1.0,
-                norm=1.0,
             )
         except CudaSupportError:
             if CUDA_AVAILABLE:
@@ -84,10 +83,8 @@ def test_scatter_mass_conservation():
 
     for resolution in resolutions:
         scatter = projection_backends["fast"]
-        image = scatter(
-            x=x, y=y, m=m, h=h, res=resolution, box_x=1.0, box_y=1.0, norm=1.0
-        )
-        mass_in_image = image.sum() / (resolution ** 2)
+        image = scatter(x=x, y=y, m=m, h=h, res=resolution, box_x=1.0, box_y=1.0)
+        mass_in_image = image.sum() / (resolution**2)
 
         # Check mass conservation to 5%
         assert np.isclose(mass_in_image.view(np.ndarray), total_mass, 0.05)
@@ -123,7 +120,6 @@ def test_scatter_parallel(save=False):
         res=resolution,
         box_x=1.0,
         box_y=1.0,
-        norm=1.0,
     )
     image_par = scatter_parallel(
         x=coordinates[0],
@@ -133,7 +129,6 @@ def test_scatter_parallel(save=False):
         res=resolution,
         box_x=1.0,
         box_y=1.0,
-        norm=1.0,
     )
 
     if save:
@@ -158,7 +153,6 @@ def test_slice(save=False):
         box_x=1.0,
         box_y=1.0,
         box_z=1.0,
-        norm=1.0,
     )
 
     if save:
@@ -199,7 +193,6 @@ def test_slice_parallel(save=False):
             box_x=1.0,
             box_y=1.0,
             box_z=1.0,
-            norm=1.0,
         )
         image_par = slice_backends_parallel[backend](
             x=coordinates[0],
@@ -213,7 +206,6 @@ def test_slice_parallel(save=False):
             box_x=1.0,
             box_y=1.0,
             box_z=1.0,
-            norm=1.0,
         )
 
         assert np.isclose(image, image_par).all()
@@ -237,7 +229,6 @@ def test_volume_render():
         box_x=1.0,
         box_y=1.0,
         box_z=1.0,
-        norm=1.0,
     )
 
     return
@@ -267,7 +258,6 @@ def test_volume_parallel():
         box_x=1.0,
         box_y=1.0,
         box_z=1.0,
-        norm=1.0,
     )
     scatter_parallel = volume_render_backends_parallel["scatter"]
     image_par = scatter_parallel(
@@ -280,7 +270,6 @@ def test_volume_parallel():
         box_x=1.0,
         box_y=1.0,
         box_z=1.0,
-        norm=1.0,
     )
 
     assert np.isclose(image, image_par).all()
@@ -346,13 +335,13 @@ def test_render_outside_region():
     h[h > 0.5] = 0.05
     m = np.ones_like(h)
     projection_backends["histogram"](
-        x=x, y=y, m=m, h=h, res=resolution, box_x=1.0, box_y=1.0, norm=1.0
+        x=x, y=y, m=m, h=h, res=resolution, box_x=1.0, box_y=1.0
     )
 
     for backend in projection_backends.keys():
         try:
             projection_backends[backend](
-                x=x, y=y, m=m, h=h, res=resolution, box_x=1.0, box_y=1.0, norm=1.0
+                x=x, y=y, m=m, h=h, res=resolution, box_x=1.0, box_y=1.0
             )
         except CudaSupportError:
             if CUDA_AVAILABLE:
@@ -372,7 +361,6 @@ def test_render_outside_region():
         box_x=1.0,
         box_y=1.0,
         box_z=1.0,
-        norm=1.0,
     )
 
     volume_render_backends_parallel["scatter"](
@@ -385,7 +373,6 @@ def test_render_outside_region():
         box_x=1.0,
         box_y=1.0,
         box_z=1.0,
-        norm=1.0,
     )
 
 
@@ -448,7 +435,7 @@ def test_comoving_versus_physical(filename):
             ):
                 img = func(data, resolution=64, project="masses", region=region)
         assert data.gas.masses.comoving and img.comoving
-        assert (img.cosmo_factor.expr - a ** aexp).simplify() == 0
+        assert (img.cosmo_factor.expr - a**aexp).simplify() == 0
         # densities are physical, make sure this works with physical coordinates and
         # smoothing lengths
         img = func(data, resolution=64, project="densities", region=region)
@@ -514,14 +501,14 @@ def test_panel_rendering(filename):
 
     plt.imsave(
         "panels_added.png",
-        plt.get_cmap()(LogNorm(vmin=10 ** 6, vmax=10 ** 6.5)(np.sum(panel, axis=-1))),
+        plt.get_cmap()(LogNorm(vmin=10**6, vmax=10**6.5)(np.sum(panel, axis=-1))),
     )
 
     projected = project_gas(data, res, "masses", backend="renormalised")
 
     plt.imsave(
         "projected.png",
-        plt.get_cmap()(LogNorm(vmin=10 ** 6, vmax=10 ** 6.5)(projected)),
+        plt.get_cmap()(LogNorm(vmin=10**6, vmax=10**6.5)(projected)),
     )
 
     fullstack = np.zeros((res, res))
@@ -575,7 +562,6 @@ def test_periodic_boundary_wrapping():
                 res=pixel_resolution,
                 box_x=boxsize,
                 box_y=boxsize,
-                norm=boxsize ** 2,
             )
             image2 = projection_backends[backend](
                 x=coordinates_non_periodic[:, 0],
@@ -585,7 +571,6 @@ def test_periodic_boundary_wrapping():
                 res=pixel_resolution,
                 box_x=0.0,
                 box_y=0.0,
-                norm=boxsize ** 2,
             )
             assert (image1 == image2).all()
         except CudaSupportError:
@@ -608,7 +593,6 @@ def test_periodic_boundary_wrapping():
             box_x=boxsize,
             box_y=boxsize,
             box_z=boxsize,
-            norm=boxsize ** 3,
         )
         image2 = slice_backends[backend](
             x=coordinates_non_periodic[:, 0],
@@ -622,7 +606,6 @@ def test_periodic_boundary_wrapping():
             box_x=0.0,
             box_y=0.0,
             box_z=0.0,
-            norm=boxsize ** 3,
         )
 
         assert (image1 == image2).all()
@@ -639,7 +622,6 @@ def test_periodic_boundary_wrapping():
         box_x=boxsize,
         box_y=boxsize,
         box_z=boxsize,
-        norm=boxsize ** 3,
     )
     image2 = scatter(
         x=coordinates_non_periodic[:, 0],
@@ -651,7 +633,6 @@ def test_periodic_boundary_wrapping():
         box_x=0.0,
         box_y=0.0,
         box_z=0.0,
-        norm=boxsize ** 3,
     )
 
     assert (image1 == image2).all()
@@ -686,11 +667,9 @@ def test_volume_render_and_unfolded_deposit():
         box_x=boxsize,
         box_y=boxsize,
         box_z=boxsize,
-        norm=boxsize ** 3,
     )
 
-    # need to divide out the box volume for the deposition
-    assert np.allclose(deposition / boxsize ** 3, volume.view(np.ndarray))
+    assert np.allclose(deposition, volume.view(np.ndarray))
 
 
 def test_folding_deposit():
@@ -729,8 +708,8 @@ def test_volume_render_and_unfolded_deposit_with_units(filename):
     # Volume render the particles
     volume = render_gas(data, npix, parallel=False).to_physical()
 
-    mean_density_deposit = (np.sum(deposition) / npix ** 3).to("Msun / kpc**3").v
-    mean_density_volume = (np.sum(volume) / npix ** 3).to("Msun / kpc**3").v
+    mean_density_deposit = (np.sum(deposition) / npix**3).to("Msun / kpc**3").v
+    mean_density_volume = (np.sum(volume) / npix**3).to("Msun / kpc**3").v
     mean_density_calculated = (
         (np.sum(data.gas.masses) / (data.metadata.boxsize[0] * data.metadata.a) ** 3)
         .to("Msun / kpc**3")
@@ -755,15 +734,15 @@ def test_dark_matter_power_spectrum(filename, save=False):
 
     min_k = cosmo_quantity(
         1e-2,
-        unyt.Mpc ** -1,
+        unyt.Mpc**-1,
         comoving=True,
-        cosmo_factor=cosmo_factor(a ** -1, data.metadata.scale_factor),
+        cosmo_factor=cosmo_factor(a**-1, data.metadata.scale_factor),
     )
     max_k = cosmo_quantity(
         1e2,
-        unyt.Mpc ** -1,
+        unyt.Mpc**-1,
         comoving=True,
-        cosmo_factor=cosmo_factor(a ** -1, data.metadata.scale_factor),
+        cosmo_factor=cosmo_factor(a**-1, data.metadata.scale_factor),
     )
 
     bins = np.geomspace(min_k, max_k, 32)
@@ -800,7 +779,7 @@ def test_dark_matter_power_spectrum(filename, save=False):
 
         folds[folding] = deposition
 
-        folding_output[2 ** folding] = (k, power_spectrum, scatter)
+        folding_output[2**folding] = (k, power_spectrum, scatter)
 
     # Now try doing them all together at once.
 
