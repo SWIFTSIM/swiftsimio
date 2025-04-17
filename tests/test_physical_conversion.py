2ff0acd8b29a14afd089c02729df7e08a9c4d31f
Kyle Oman <kyle.a.oman@durham.ac.uk>
2025-04-17 11:01:07 +0100

    Add simultaneous physical and unit conversion (#224)

--- tests/test_physical_conversion.py
+++ tests/test_physical_conversion.py
@@ -1,21 +1,44 @@
 from tests.helper import requires
-from swiftsimio import load, cosmo_array
-from numpy import array_equal
+import unyt as u
+from swiftsimio import load
+from numpy import allclose
 
 
 @requires("cosmological_volume.hdf5")
 def test_convert(filename):
     """
-    Check that the conversion to physical units is done correctly
+    Check that the conversion to physical units is done correctly.
     """
     data = load(filename)
     coords = data.gas.coordinates
-    units = coords.units
+    units = u.kpc
+    assert units != coords.units  # ensure we make a non-trivial conversion
+    assert data.metadata.a != 1.0  # ensure we make a non-trivial conversion
     coords_physical = coords.to_physical()
 
     # array_equal applied to cosmo_array's is aware of physical & comoving
     # make sure to compare bare arrays:
-    assert array_equal(
-        coords.to_value(units) * data.metadata.a, coords_physical.to_value(units)
+    assert allclose(
+        coords.to_value(units) * data.metadata.a,
+        coords_physical.to_value(units),
+        rtol=1e-6,
     )
     return
+
+
+@requires("cosmological_volume.hdf5")
+def test_convert_to_value(filename):
+    """
+    Check that conversions to numerical values are correct.
+    """
+    data = load(filename)
+    coords = data.gas.coordinates
+    units = u.kpc
+    assert units != coords.units  # ensure we make a non-trivial conversion
+    assert data.metadata.a != 1.0  # ensure we make a non-trivial conversion
+    coords_physical_values = coords.to_physical_value(units)
+    coords_comoving_values = coords.to_comoving_value(units)
+    print(coords_physical_values / (coords_comoving_values * data.metadata.a))
+    assert allclose(
+        coords_physical_values, coords_comoving_values * data.metadata.a, rtol=1e-6
+    )
