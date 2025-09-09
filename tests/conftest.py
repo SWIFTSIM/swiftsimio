79cf5e4ab8398ab402ec28f1204b4cdab3b95455
John Helly <j.c.helly@durham.ac.uk>
2025-09-09 10:48:41 +0100

    Fixes to test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,13 +61,18 @@ def cosmological_volume_only_distributed():
 def cosmological_volume_dithered():
     yield _requires("LegacyCosmologicalVolumeDithered.hdf5")
 
+
 #
 # Allow enabling remote file tests with a command line flag
 #
 def pytest_addoption(parser):
     parser.addoption(
-        "--enable-hdfstream-tests", action="store_true", default=False, help="Run tests which access files using the hdfstream module"
+        "--enable-hdfstream-tests",
+        action="store_true",
+        default=False,
+        help="Run tests which access files using the hdfstream module",
     )
+
 
 #
 # Fixtures for tests of remote data access with hdfstream:
@@ -75,43 +80,57 @@ def pytest_addoption(parser):
 #
 server = "https://dataweb.cosma.dur.ac.uk:8443/hdfstream"
 server_test_data_path = "Tests/SWIFT/IOExamples/ssio_ci_04_2025"
+
+
 def test_data_parameters(request):
     if "server" in request.param:
         if not request.config.getoption("--enable-hdfstream-tests"):
             pytest.skip("Skipping remote tests: --enable-hdfstream-tests not set")
         return request.param
     else:
-        return {"filename" : _requires(request.param["filename"])}
+        return {"filename": _requires(request.param["filename"])}
+
 
 # Fixture which returns load parameters for the cosmological volume
 @pytest.fixture(
     params=[
         # Local files
-        {"filename" : f"EagleDistributed.hdf5"},
-        {"filename" : f"EagleSingle.hdf5"},
-        {"filename" : f"LegacyCosmologicalVolume.hdf5"},
+        {"filename": f"EagleDistributed.hdf5"},
+        {"filename": f"EagleSingle.hdf5"},
+        {"filename": f"LegacyCosmologicalVolume.hdf5"},
         # Remote files
-        {"filename" : f"{server_test_data_path}/EagleDistributed.hdf5", "server" : server},
-        {"filename" : f"{server_test_data_path}/EagleSingle.hdf5", "server" : server},
-        {"filename" : f"{server_test_data_path}/LegacyCosmologicalVolume.hdf5", "server" : server},
+        {
+            "filename": f"{server_test_data_path}/EagleDistributed.hdf5",
+            "server": server,
+        },
+        {"filename": f"{server_test_data_path}/EagleSingle.hdf5", "server": server},
+        {
+            "filename": f"{server_test_data_path}/LegacyCosmologicalVolume.hdf5",
+            "server": server,
+        },
     ]
 )
 def cosmological_volume_params(request):
     return test_data_parameters(request)
 
+
 @pytest.fixture(
     params=[
-        {"filename" : f"LegacyCosmologicalVolumeDithered.hdf5"},
-        {"filename" : f"{server_test_data_path}/LegacyCosmologicalVolumeDithered.hdf5", "server" : server},
+        {"filename": f"LegacyCosmologicalVolumeDithered.hdf5"},
+        {
+            "filename": f"{server_test_data_path}/LegacyCosmologicalVolumeDithered.hdf5",
+            "server": server,
+        },
     ]
 )
 def cosmological_volume_dithered_params(request):
     return test_data_parameters(request)
 
+
 @pytest.fixture(
     params=[
-        {"filename" : f"SoapExample.hdf5"},
-        {"filename" : f"{server_test_data_path}/SoapExample.hdf5", "server" : server},
+        {"filename": f"SoapExample.hdf5"},
+        {"filename": f"{server_test_data_path}/SoapExample.hdf5", "server": server},
     ]
 )
 def soap_example_params(request):
